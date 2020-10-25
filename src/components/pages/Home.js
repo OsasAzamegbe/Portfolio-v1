12fd5c74a75605a77b4c0ea5df9db9a1435f966a
osamudiamen <osas.azamegbe@gmail.com>
2020-10-25 16:07:56 +0100

    CHORE: edited body css. created Home page component.

---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <header>Home</header>
+        </div>
+    )
+}
+
+export default Home
