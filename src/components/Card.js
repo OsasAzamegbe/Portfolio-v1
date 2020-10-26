fbc34524941a8796af3e8a84f4eb52cfea5505a9
osamudiamen <osas.azamegbe@gmail.com>
2020-10-26 13:21:01 +0100

    FEAT: Added card links.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Card.css'
+import {Link} from 'react-router-dom'
 
 
 const Card = () => {
@@ -8,7 +9,16 @@ const Card = () => {
             <div className="card-container">
                 <header className="card-heading">URL Shortening Service</header>
                 <p className="card-description">URL SHORTENER that takes URLs and gives users shortened versions for easier use. The shortened URLs redirect to the original long URL when accessed through a browser. Built with the MERN stack. MongoDB, Node.js, React and Express.</p>
+                <div className="card-links-container">
+                    <Link target="_blank" to="//github.com/OsasAzamegbe/Url-Shortening-Service">
+                        Code Base
+                    </Link>
+                    <Link target="_blank" to="//sh-orten.herokuapp.com/">
+                        Live Version
+                    </Link>
+                </div>
             </div>
+            
         </div>
     )
 }
