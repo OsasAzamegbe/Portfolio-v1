9aea291f172e1c45a3c39e9c709081287a42cc98
osamudiamen <osas.azamegbe@gmail.com>
2020-10-29 22:03:28 +0100

    FEAT: added styling and text content to About component.

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -5,7 +5,26 @@ import './About.css'
 const About = () => {
     return (
         <div className="about">
-            <header>About Me</header>
+            <div className="about-quotes-container">
+                <p className="about-quotes">"A clever person <strong>solves</strong> a problem.
+                <br/>A wise person <strong>avoids</strong> it."</p>
+                <p className="about-quotes-author"><i>- Albert Einstein</i></p>
+            </div>
+            <div className="about-content-container">
+                <p className="about-content">
+                    Osas Azamegbe is a Software Engineer - a creative problem-solver - 
+                    with a firm grasp of engineering concepts, data structures, 
+                    systems architecture, and algorithm analysis and design.
+                    <br/> 
+                </p>
+                <p className="about-content-extra">Artist. Engineer.</p>
+            </div>
+            <div className="tech-container">
+                <header className="tech-header"><p>Technologies</p></header>
+                <div className="tech-icons">
+
+                </div>
+            </div>
         </div>
     )
 }
