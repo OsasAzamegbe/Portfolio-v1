b04396960f5132041d87d285b5967e9982ec0530
osamudiamen <osas.azamegbe@gmail.com>
2020-10-31 23:35:46 +0100

    CHORE: Refactored card component. removed active state.

--- Client/src/components/Card.js
+++ Client/src/components/Card.js
@@ -1,38 +1,30 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './Card.css'
 import {Link} from 'react-router-dom'
 import * as techIcons from './Icons'
 
 
 const Card = ({headingText, descriptionText, codeBaseHref, liveVersionHref, techStack}) => {
-
-    const [active, setActive] = useState(true)
-
     return(
-        <div>
-            { active ?
-                <div className="card">
-                    <img src="images/urlShortening.png" alt="URL Shortening Service" className="card-img"/>            
-                </div>
-                :
-                <div className="card card-pad">
-                    <div className="card-container">
-                        <header className="card-heading">{headingText}</header>
-                        <p className="card-description">{descriptionText}</p>
-                        <div className="card-links-container">
-                            <Link className="card-link" target="_blank" to={codeBaseHref}>
-                                Code Base
-                            </Link>
-                            <Link className="card-link" target="_blank" to={liveVersionHref}>
-                                Live Version
-                            </Link>
-                        </div>
-                        <div className="card-techstack">
-                            {techStack.map(icon => techIcons[icon].apply())}
-                        </div>
-                    </div>            
-                </div>
-            }        
+        <div style={{backgroundImage: "images/urlShortening.png"}} className="card-wrapper" >
+            <div className="card card-pad">
+                <div className="card-container">
+                    <header className="card-heading">{headingText}</header>
+                    <p className="card-description">{descriptionText}</p>
+                    <div className="card-links-container">
+                        <Link className="card-link" target="_blank" to={codeBaseHref}>
+                            Code Base
+                        </Link>
+                        <Link className="card-link" target="_blank" to={liveVersionHref}>
+                            Live Version
+                        </Link>
+                    </div>
+                    <div className="card-techstack">
+                        {techStack.map(icon => techIcons[icon].apply())}
+                    </div>
+                </div>            
+            </div>
+                   
         </div>
     )
 }
