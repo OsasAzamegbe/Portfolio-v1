18a4db19fd78f1ba436821451a9a184b73df2fdb
osamudiamen <osas.azamegbe@gmail.com>
2020-10-31 09:57:09 +0100

    FEAT: added states to form input fields and a form submit handler.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,29 +1,83 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './ContactForm.css'
+import axios from 'axios'
 
 
 const ContactForm = () => {
+
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [title, setTitle] = useState("")
+    const [message, setMessage] = useState("")
+
+    const nameHandler = (e) => {
+        setName(e.target.value)
+    }
+
+    const emailHandler = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const titleHandler = (e) => {
+        setTitle(e.target.value)
+    }
+
+    const messageHandler = (e) => {
+        setMessage(e.target.value)
+    }
+
+    const resetForm = () => {
+        setName("")
+        setEmail("")
+        setTitle("")
+        setMessage("")
+    }
+
+    const submitHandler = async (e) => {
+        e.preventDefault()
+
+        const response = await axios({
+            method: "POST",
+            url: "/send",
+            data: {                
+                name, email, title, message
+            }
+        })
+
+        if (response.data.status === 'success'){
+            alert("Message Sent."); 
+            resetForm()
+        }else if(response.data.status === 'fail'){
+            alert("Message failed to send.")
+        }
+
+    }
+
+
     return (
         <div className="contactform">
             <form className="contactform-form">
                 <div className="form-item">
                     <label className="form-label">Name</label>
-                    <input name="name" id="name" type="text" placeholder="Your Name" className="form-input"/>
+                    <input value={name} onChange={nameHandler} required name="name" 
+                    id="name" type="text" placeholder="Your Name" className="form-input"/>
                 </div>
                 <div className="form-item">
                     <label className="form-label">Email</label>
-                    <input name="email" id="email" type="email" placeholder="Your Email" className="form-input"/>
+                    <input value={email} onChange={emailHandler} required name="email" 
+                    id="email" type="email" placeholder="Your Email" className="form-input"/>
                 </div>
                 <div className="form-item">
                     <label className="form-label">Title</label>
-                    <input name="title" id="title" type="text" placeholder="Title" className="form-input"/>
+                    <input value={title} onChange={titleHandler} name="title" id="title" type="text" placeholder="Title" className="form-input"/>
                 </div>
                 <div className="form-item">
                     <label className="form-label">Message</label>
-                    <textarea rows="10" cols="25" name="message" id="message" type="text" placeholder="Message" className="form-input"/>
+                    <textarea value={message} onChange={messageHandler} required 
+                    rows="10" cols="25" name="message" id="message" type="text" placeholder="Message" className="form-input"/>
                 </div>
                 <div className="form-item form-item-btn">
-                    <button className="btn form-btn" type="submit">Send</button>
+                    <button onSubmit={submitHandler} className="btn form-btn" type="submit">SEND</button>
                 </div>
             </form>
         </div>
