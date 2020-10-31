fff5b8232b31666497e225a455059b48a82a97be
osamudiamen <osas.azamegbe@gmail.com>
2020-10-31 13:25:52 +0100

    CHORE: created access tokens

--- Server/config/googleAuthConfig.js
+++ Server/config/googleAuthConfig.js
@@ -1,12 +1,12 @@
-import * as googleAuth from 'google-auth-library';
-import { Credentials } from 'google-auth-library/build/src/auth/credentials';
+const googleAuth = require('google-auth-library');
+const Credentials = require('google-auth-library/build/src/auth/credentials');
 const dotenv = require('dotenv')
 dotenv.config()
 
 const scope = "https://mail.google.com/";
 
 
-export const credentials = {
+const credentials = {
     "web": {
         "client_id": process.env.CLIENT_ID,
         "project_id":"nodemailer-294211",
@@ -18,3 +18,52 @@ export const credentials = {
 		"javascript_origins": ["http://localhost"]
     }
 }
+
+/**
+ * Step 1: Authorize in the browser
+ */
+
+const getAuthorizeUrl = (callback) => {
+	const oauth2Client = new googleAuth.OAuth2Client(credentials.web.client_id, credentials.web.client_secret, credentials.web.redirect_uris[0]);
+
+	const authUrl = oauth2Client.generateAuthUrl({
+		access_type: 'offline',
+		scope: scope
+	});
+
+	callback(null, authUrl);
+}
+
+// getAuthorizeUrl((err, url) => {
+// 	if(err) return console.log(err);
+// 	console.log("Auth url is: ", url);
+// });
+
+
+const code = process.env.AUTH_URI
+
+const getAccessToken = (callback) => {
+	const oauth2Client = new googleAuth.OAuth2Client(credentials.web.client_id, credentials.web.client_secret, credentials.web.redirect_uris[0]);
+
+	oauth2Client.getToken(code, (err, token) => {
+		if(err) return console.log(err);
+
+		callback(null, token);
+	});
+}
+
+// getAccessToken((err, token) => {
+// 	if(err) return console.log(err);
+// 	console.log("Auth token is: ", token);
+// });
+
+
+const tokens = {
+    access_token: process.env.ACCESS_TOKEN,
+    refresh_token: process.env.REFRESH_TOKEN,
+    scope: 'https://mail.google.com/',
+    token_type: 'Bearer',
+    expiry_date: 1604150529866
+  }
+
+module.exports = {credentials, getAuthorizeUrl, getAccessToken, tokens}
