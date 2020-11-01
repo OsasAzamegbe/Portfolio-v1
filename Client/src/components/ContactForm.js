6a4698a1bbc6d77cb3a0f78a5343b1f002ba3c0f
osamudiamen <osas.azamegbe@gmail.com>
2020-11-01 20:05:21 +0100

    CHORE: Connected labels.

--- Client/src/components/ContactForm.js
+++ Client/src/components/ContactForm.js
@@ -61,21 +61,21 @@ const ContactForm = () => {
         <div className="contactform">
             <form  onSubmit={submitHandler} className="contactform-form">
                 <div className="form-item">
-                    <label className="form-label">Name</label>
+                    <label for="name" className="form-label">Name</label>
                     <input value={name} onChange={nameHandler} required name="name" 
                     id="name" type="text" placeholder="Your Name" className="form-input"/>
                 </div>
                 <div className="form-item">
-                    <label className="form-label">Email</label>
+                    <label for="email" className="form-label">Email</label>
                     <input value={email} onChange={emailHandler} required name="email" 
                     id="email" type="email" placeholder="Your Email" className="form-input"/>
                 </div>
                 <div className="form-item">
-                    <label className="form-label">Title</label>
+                    <label for="title" className="form-label">Title</label>
                     <input value={title} onChange={titleHandler} name="title" id="title" type="text" placeholder="Title" className="form-input"/>
                 </div>
                 <div className="form-item">
-                    <label className="form-label">Message</label>
+                    <label for="message" className="form-label">Message</label>
                     <textarea value={message} onChange={messageHandler} required 
                     rows="10" cols="25" name="message" id="message" type="text" placeholder="Message" className="form-input"/>
                 </div>
